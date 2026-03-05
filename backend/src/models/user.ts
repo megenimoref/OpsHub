3538d8f01a8b437b4ae25d74e121edc869125127
shlomiazu <shlomi.a82@gmail.com>
2026-03-05 16:28:16 +0200

    shlomichang

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -103,6 +103,12 @@ User.init(
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(user.password, salt);
       },
+      beforeUpdate: async (user) => {
+        if (user.changed('password')) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      },
     },
   }
 );
